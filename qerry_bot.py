f90bf24779ab90abb2334e9c057a592d1b84ae4b
Dmitriy Boger <kotvberloge@gmail.com>
2016-10-22 20:54:18 +0300

    Update qerry_bot.py

--- qerry_bot.py
+++ qerry_bot.py
@@ -6,30 +6,34 @@ from parser import get_schedule
 bot = telebot.TeleBot(config.token)
 list_links = json.load(open('documents/links.json','r',encoding='utf-8'))
 
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    print(message.chat.id, message.chat.username) # вместо print(one + ' ' + two)
+    text = 'long long long'
+    try:
+        msg = "привет {0.first_name} {0.last_name} {1}".format(message.chat, text)
+    except:
+        msg = "привет {0.username} {1}".format(message.chat, text)
+    bot.send_message(message.chat.id, msg)
+    
+@bot.message_handler(commands=['schedule'])
+    bot.send_message(message.chat.id, "schedule")
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     print("alla")
     bot.send_chat_action(message.chat.id,'typing')
     print(list_links['groups'])
-    if(message.text == "/start"):
-        print(str(message.chat.id) + " " + message.chat.username + " ")
-        text = 'long long long'
-        try:
-            bot.send_message(message.chat.id, "привет " + message.chat.first_name + " " + message.chat.last_name + text)
-        except:
-            bot.send_message(message.chat.id, "привет " + message.chat.username + text)
-    if (message.text == "/schedule"):
-        bot.send_message(message.chat.id, "schedule")
+    
     if (message.text.strip().lower() in list_links['groups']):
         msg = get_schedule(message.text.strip().lower(),0)
         bot.send_message(message.chat.id, msg)
     elif (message.text.strip().lower() in list_links['lecturers']):
         msg = get_schedule(message.text.strip().lower(),1)
         bot.send_message(message.chat.id, msg)
-
-
-    print("user: " + str(message.chat.username) + " chat: " + str(message.chat.id))
+    
+    print("user: %s chat: %s" % (message.chat.username, message.chat.id)) 
+    # или print("user: {0.username} chat: {0.id}".format(message.chat))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
