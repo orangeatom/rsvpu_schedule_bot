e526c24219e8efb4817fe1c63a3cf8227640ba3a
orangeatom <antonpaly@ya.ru>
2016-10-25 21:41:19 +0500

    addet_database

--- query_bot.py
+++ query_bot.py
@@ -7,30 +7,38 @@ from peewee import *
 
 user_base = SqliteDatabase('documents/users.db')
 
-
-
 bot = telebot.TeleBot(config.token)
 queries = json.load(open('documents/links.json', 'r', encoding='utf-8'))
-bd = SqliteDatabase('users.db')
+
 Weekdays = ('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье')
-
-
-class User(Model):
-    id = CharField(unique=True)
-    status = IntegerField(null=True)
-    class Meta:
-        database = user_base
 
 class state:
     null = 0
     gpoup = 1
-    select_group = 2
-    waiting_answer = 3
+    select = 2
+    waiting_group = 3
+    waiting_lecturer = 4
+    pass
+
+class User(Model):
+    User_id = CharField(unique=True)
+    State = IntegerField(null=True)
+    # this data need to query to schedule
+    Group_id = CharField(null=True)
+    Lecturer_id = CharField(null=True)
+
+    Distribution = IntegerField(null=True)
+    class Meta:
+        database = user_base
+
+
+def search():
     pass
 
 
 @bot.message_handler(commands=['start'])
 def hello_message(message):
+    usr = User.create_or_get(User_id = message.chat.id, State = state.null)
     bot.send_chat_action(message.chat.id, 'typing')
     bot.send_message(message.chat.id, "Приветствую, я неинтеллектуально обученная система по рассылке расписания \n Вы можете ввести название группы или часть от ФИО преподавателя "
                                       "и получить его расписание на сегодня (до 18 00) и на завтра (после 18 00)")
@@ -38,18 +46,21 @@ def hello_message(message):
 @bot.message_handler(commands=['set_group'])
 def add_user(message):
     bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, "Введите нужную группу или имя")
-
-    pass
+    zzz = User.get_or_create(User_id = message.chat.id, State = state.select)
+    print(zzz[0].State)
+    markup = telebot.types.ReplyKeyboardMarkup()
+    markup.row('Группа')
+    markup.row('Преподаватель')
+    bot.send_message(message.chat.id, "выберите тип необходимого расписания",reply_markup=markup)
 
 @bot.message_handler(commands=['schedule'])
 def send_schedule_today(message):
     bot.send_chat_action(message.chat.id,'typing')
     msg = ""
-
-
     bot.send_message(message.chat.id, msg)
     pass
+
+@bot.message_handler(commands=['cansel'])
 
 @bot.message_handler(commands=['schedule_t'])
 def send_schedule_tomorrow(message):
@@ -69,22 +80,45 @@ def set_distribution(message):
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_chat_action(message.chat.id,'typing')
-    if (message.text.strip().lower() in queries['groups']):
-        text = '{0} \n{1}\n'.format(Weekdays[datetime.date.today().weekday()], datetime.date.today())
-        msg = parser.get_schedule_today(message.text.strip().lower(), 0)
-        for lection in range(0,7):
-            #print(msg[0][lection][0] + " " + msg[0][lection][1])
-            text += '{0} {1}\n'.format(msg[0][lection][0] , msg[0][lection][1])
+    #print(User.select().where(User.Id == message.chat.id and User.State == state.select))
+    user = User.get(User.User_id == message.chat.id)
+    if user.State == state.select:
+        if message.text == 'Группа':
+            markup =  telebot.types.ReplyKeyboardHide()
+            bot.send_message(user.User_id, 'Введите название группы: ',reply_markup = markup)
+        elif message.text == 'Преподаватель':
+            markup = telebot.types.ReplyKeyboardHide()
+            bot.send_message(user.User_id, 'Введите имя преподавателя: ',reply_markup= markup)
+        else:
+            bot.send_message(user.User_id, 'Выберите элемент из списка')
 
-        bot.send_message(message.chat.id, text)
-    elif (message.text.strip().lower() in queries['lecturers']):
-        msg = parser.get_schedule_today(message.text.strip().lower(), 1)
-        bot.send_message(message.chat.id, msg)
+    elif user.State == state.null:
+        if (message.text.strip().lower() in queries['groups']):
+            try:
+                text = '{0} \n{1}\n'.format(Weekdays[datetime.date.today().weekday()], datetime.date.today())
+                msg = parser.get_schedule_today(message.text.strip().lower(), 0)
+                for lection in range(0,7):
+                    #print(msg[0][lection][0] + " " + msg[0][lection][1])
+                    text += '{0} {1}\n'.format(msg[0][lection][0] , msg[0][lection][1])
+
+                bot.send_message(message.chat.id, text)
+            except:
+                pass
+        elif (message.text.strip().lower() in queries['lecturers']):
+            try:
+                text = '{0} \n{1}\n'.format(Weekdays[datetime.date.today().weekday()], datetime.date.today())
+                msg = parser.get_schedule_today(message.text.strip().lower(), 1)
+                for lection in range(0,7):
+                    #print(msg[0][lection][0] + " " + msg[0][lection][1])
+                    text += '{0} {1}\n'.format(msg[0][lection][0] , msg[0][lection][1])
+                bot.send_message(message.chat.id, text)
+            except:
+                pass
 
 
 if __name__ == '__main__':
     user_base.connect()
-    user_base.create_table()
+    user_base.create_tables([User], safe=True)
     user_base.close()
     bot.polling(none_stop=True)
 
